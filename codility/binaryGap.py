af469552aeb22253202a1a24d3f3da580911528f
Gavaskar Kangaraj <gavaskar2018@gmail.com>
2021-05-13 07:17:09 +0200

    update binaryGap exercise

--- codility/binaryGap.py
+++ codility/binaryGap.py
@@ -1,6 +1,6 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
-
+# https://codility.com/media/train/Iterations.pdf
 def solution(N):
     # write your code in Python 3.6
     
